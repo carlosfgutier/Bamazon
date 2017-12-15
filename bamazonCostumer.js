e3cadd20fb7f03453f4aaba01613b16e26540126
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-15 11:24:37 -0600

    Created home functionwith Log Out option

--- bamazonCostumer.js
+++ bamazonCostumer.js
@@ -13,9 +13,26 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
 	if (err) throw err;
-	// console.log("connected as id " + connection.threadId + "\n");
-	shop();
+	home();
 });
+
+function home() {
+	inquirer.prompt([
+		{
+			name: 'order',
+			type: 'list',
+			message: 'What would you like to do?',
+			choices: ['Shop', 'Log out']
+		}
+	]).then(function(answer) {
+		if (answer.order == 'Shop') {
+			shop();
+		} else {
+			connection.end();
+			console.log('\nThank you for shopping with us!\n');
+		}
+	});
+}
 
 function shop() {
 	console.log('\nItems for sale\n');
@@ -48,38 +65,13 @@ function shop() {
 					inStock = res[(answer.itemId - 1)].stock_quantity;
 	    			checkout(answer.itemId, answer.itemUnits);
 	    		} else {
-	    			shop();
 	    			console.log('\nPlease answer all fields\n');
+	    			shopMore();
 	    		}
 			});
 		}
 	});
 };
-
-// function afterConnection(){
-// 	console.log("Stocked Items\n");
-//   	connection.query("SELECT * FROM products", function(err, res) {
-    
-// 	    if (err) throw err;
-// 	    console.log(res);
-
-// 	    inquirer.prompt([
-// 	    	{
-// 	    		name: 'itemId',
-// 	    		type: 'input',
-// 	    		message: '\nType the ID of the item you would like to buy'
-// 	    	},
-// 	    	{
-// 	    		name: 'itemUnits',
-// 	    		type: 'input',
-// 	    		message: 'How many units would you like to buy?'
-// 	    	}
-// 	    ]).then(function(answer) {
-// 	    	inStock = res[(answer.itemId - 1)].stock_quantity;
-// 	    	checkout(answer.itemId, answer.itemUnits);
-// 	    });
-//   	});
-// };
 
 function checkout(item, quantity) {
 	if (quantity <= inStock) {
@@ -110,7 +102,7 @@ function shopMore() {
 			shop();
 		} else {
 			connection.end();
+			console.log('\nThank you for shopping with us!\n');
 		}
 	});
 };
-
